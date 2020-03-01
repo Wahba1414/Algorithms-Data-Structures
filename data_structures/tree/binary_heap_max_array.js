c613e222fdc9461fb25b8a102b6a503614425d75
Zizo1414 <zizo@makeitsimple.co.uk>
2020-03-01 19:44:44 +0200

    useful comments for Binary Max Heap

--- data_structures/tree/binary_heap_max_array.js
+++ data_structures/tree/binary_heap_max_array.js
@@ -2,8 +2,7 @@ class MaxBinaryHeap {
   constructor() {
     this.data = [];
   }
-
-
+  //O(log(n))
   insert(value) {
     if (this.data.length == 0) {
       this.data.push(value);
@@ -12,12 +11,14 @@ class MaxBinaryHeap {
       this.bubbleUp(this.data.length - 1);
     }
   }
-
-  // Helper functions.
+  /*
+  It compares the new inserted item against its parent until it reaches the 
+  most upper node to maintain this heap a maximum one.
+  */
   bubbleUp(index) {
     if (index > 0) {
+      //the equation to get the parent for some index in the array.
       let parentIndex = Math.floor((index - 1) / 2);
-
       if (this.data[index] > this.data[parentIndex]) {
         // swap.
         [this.data[index], this.data[parentIndex]] = [this.data[parentIndex], this.data[index]];
@@ -30,6 +31,7 @@ class MaxBinaryHeap {
     if (this.data.length) return this.data[0];
   }
 
+  // log(n)
   extractMax() {
     if (this.data.length == 0) {
       return null;
@@ -40,6 +42,8 @@ class MaxBinaryHeap {
     }
 
     var max = this.data[0];
+
+    // Remove the last item and use its value to replace the root node.
     this.data[0] = this.data.pop();
 
     this.bubbleDown(0);
@@ -47,40 +51,36 @@ class MaxBinaryHeap {
     return max;
   }
 
-  // helper function.
+  /*It compares the new value of the root item against its children until it reaches 
+  the most lower node to maintain this heap a maximum one.*/
   bubbleDown(index) {
     if (index < this.data.length - 1) {
+      // Equations to get children indexes for some parent (array index).
       var [child1, child2] = [this.data[2 * index + 1], this.data[2 * index + 2]];
-
       if ((this.data[index] < child1) && (this.data[index] < child2)) {
-        // greater than both children.
-        // get the min. and replace with it.
         if (child1 > child2) {
           // swap.
           [this.data[2 * index + 1], this.data[index]] = [this.data[index], this.data[2 * index + 1]];
           this.bubbleDown(2 * index + 1);
-
         } else if (child2 > child1) {
           //swap
           [this.data[2 * index + 2], this.data[index]] = [this.data[index], this.data[2 * index + 2]];
           this.bubbleDown(2 * index + 2);
-
         }
       } else if (this.data[index] < child1) {
         // swap.
         [this.data[2 * index + 1], this.data[index]] = [this.data[index], this.data[2 * index + 1]];
         this.bubbleDown(2 * index + 1);
-
       } else if (this.data[index] < child2) {
         //swap
         [this.data[2 * index + 2], this.data[index]] = [this.data[index], this.data[2 * index + 2]];
         this.bubbleDown(2 * index + 2);
-
       }
 
     }
   }
 
+  // Heap sorting. (nlog(n))
   sort() {
     while (this.data.length) {
       console.log(this.extractMax());
