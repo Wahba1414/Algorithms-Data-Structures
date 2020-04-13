ad0a9de25cc094510bd5f55280e7c0a9a9ec84d3
Zizo1414 <zizo@makeitsimple.co.uk>
2020-04-13 18:12:14 +0200

    contiguous array for binary numbers

---
+++ leetcode/contigous_array.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxLength = function (nums) {
+  // Special case
+  var tracking = {};
+  var maxLen = 0;
+  var counter = 0;
+
+  nums.forEach(function (num, index) {
+    counter += (num == 1) ? 1 : -1;
+
+    if (counter == 0) {
+      maxLen = Math.max(maxLen, index + 1);
+    } else if (counter in tracking) {
+      maxLen = Math.max(maxLen, index - tracking[counter]);
+    } else {
+      tracking[counter] = index;
+    }
+  });
+
+  return maxLen;
+
+};
+
+var nums = [0, 1];
